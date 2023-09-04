f1367355ccb05068e39709eaf2fa01c0f488fe2a
sagit <sagitgaysin87@gmail.com>
2023-09-04 20:04:42 +0300

    Сделал курсовую базовой сложности. Проверьте пожалуйста

--- src/pro/sky/java/course1/CourseWork1/Employee.java
+++ src/pro/sky/java/course1/CourseWork1/Employee.java
@@ -1,4 +1,104 @@
 package pro.sky.java.course1.CourseWork1;
 
 public class Employee {
+    private String employeeName;
+    private int department;
+    private double employeeSalary;
+    private  int employeeID;
+    private static int count = 1;
+
+    public Employee(String employeeName, int department, double salary) {
+        this.employeeName = employeeName;
+        this.department = department;
+        this.employeeSalary = salary;
+        this.setEmployeeID(count++);
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public double getEmployeeSalary() {
+        return employeeSalary;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setEmployeeSalary(int employeeSalary) {
+        this.employeeSalary = employeeSalary;
+    }
+
+    public String toString() {
+        return "Сотрудник: " + employeeName + "\nДепартамент - " + department + "\nЗарплата сотрудника - " + employeeSalary +
+                "\nID сотрудника - " + employeeID + "\n";
+    }
+
+    public static void employeesFull(Employee[] employees) {
+        for (Employee i : employees) {
+            if (employees != null) {
+                System.out.println(i);
+
+            }
+        }
+    }
+
+    public static void separator() {
+        System.out.println("----------------------------------");
+    }
+
+    public static double employeeTotalSalary(Employee[] employees) {
+        double totalSalary = 0;
+        for (Employee employee : employees) {
+            totalSalary += employee.getEmployeeSalary();
+        }
+        return totalSalary;
+    }
+
+    public static double averageSalary(Employee[] employees) {
+        return employeeTotalSalary(employees) / employees.length;
+    }
+
+    public static Employee employeeMaxSalary(Employee[] employees) {
+        Employee maxSalary = employees[0];
+        for (Employee i : employees) {
+            if (i.getEmployeeSalary() > maxSalary.getEmployeeSalary()) {
+                maxSalary = i;
+            }
+        }
+        return maxSalary;
+    }
+
+    public static Employee empMinSalary(Employee[] employees) {
+        Employee minSalary = employees[0];
+        for (Employee i : employees) {
+            if (i.getEmployeeSalary() < minSalary.getEmployeeSalary()) {
+                minSalary = i;
+            }
+        }
+        return minSalary;
+    }
+
+    public static void employeeList(Employee[] employees) {
+        for (Employee i : employees) {
+            System.out.println(i.getEmployeeName());
+        }
+    }
 }
--- src/pro/sky/java/course1/CourseWork1/Main.java
+++ src/pro/sky/java/course1/CourseWork1/Main.java
@@ -2,7 +2,33 @@ package pro.sky.java.course1.CourseWork1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+        Employee[] employees = new Employee[10];
 
+        employees[0] = new Employee("Петров Петр Петрович", 2, 65_000);
+        employees[1] = new Employee("Сидоров Сидор Сидорович", 3, 68_000);
+        employees[2] = new Employee("Иванов Иван Иванович", 1, 75_070);
+        employees[3] = new Employee("Валентинова Валентина Валентиновна", 3, 85_700);
+        employees[4] = new Employee("Петрова Ирина Петровна", 1, 67_000);
+        employees[5] = new Employee("Харитонов Харитон Харитонович", 4, 74_800);
+        employees[6] = new Employee("Серова Ольга Артуровна", 3, 78_000);
+        employees[7] = new Employee("Алексеев Алексей Алексеевич", 5, 80_099);
+        employees[8] = new Employee("Андреев Андрей Андреевич", 5, 69_000);
+        employees[9] = new Employee("Ольгина Ольга Сидоровна", 4, 84_000);
+
+        Employee.employeesFull(employees);
+        Employee.employeeTotalSalary(employees);
+
+        System.out.println("Сумма затрат на заработную плату сотрудников - " + Employee.employeeTotalSalary(employees) + "руб");
+        Employee.separator();
+        System.out.println("C максимальной зарплатой " + Employee.employeeMaxSalary(employees));
+        Employee.separator();
+        System.out.println("C наименьшей зарплатой " + Employee.empMinSalary(employees));
+        Employee.separator();
+        System.out.println("Среднее значение зарплат - " + Employee.averageSalary(employees) + "руб");
+        Employee.separator();
+
+        Employee.employeeList(employees);
+    }
 }
+
+
